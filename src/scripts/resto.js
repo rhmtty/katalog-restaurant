41ac8e1b2ae6ff99ddec87032f396564479094c7
GrimmLck <rhmtofficial@gmail.com>
2021-05-21 14:44:18 +0700

    fix artikel

--- src/scripts/resto.js
+++ src/scripts/resto.js
@@ -6,10 +6,10 @@ const getRestaurants = () => {
     
         post += `
             <article class="resto-item">
-                <img class="img-thumbnail" src="${restaurant.pictureId}" alt="thumbnail restoran">
+                <a href="#"><img class="img-thumbnail" src="${restaurant.pictureId}" alt="thumbnail restoran"></a>
 
                 <p class="city">${restaurant.city}</p>
-                <span class="rating">5.0</span>
+                <span class="rating">${restaurant.rating}</span>
                 <div class="content">
                     <h1 class="title"><strong><a href="#">${restaurant.name}</a></strong></h1>
                     <p class="description">${restaurant.description}</p>
