b5791adcc48ab2ca0998cb6b29a478933f8f8711
GrimmLck <rhmtofficial@gmail.com>
2021-05-22 14:47:34 +0700

    add menu-mobile, fix index.js, responsive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
-import '../styles/style.scss';
-import getRestaurants from './resto'
+import '../styles/responsive.scss';
+import main from './main';
 
 console.log('Hello Coders! :)'); 
 
@@ -9,19 +9,6 @@ console.log('Hello Coders! :)');
  * Muncukan navbar ketika halaman di scroll ke bawah
  * 
  **/ 
-window.addEventListener('scroll', () => showNavbar())
-
-
-// FUngsi menampilkan navbar ketika di scroll
-const showNavbar = () => {
-    const header = document.querySelector('header')
-    
-    if(document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        header.classList.add('header-scroll')
-    } else {
-        header.classList.remove('header-scroll')
-    }
-}
 
 // Menampilkan konten
-getRestaurants()
+document.addEventListener('DOMContentLoaded', main)
