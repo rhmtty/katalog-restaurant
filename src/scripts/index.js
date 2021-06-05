be165cdbdc73d79cca5a8b55a3fe3d139763a876
GrimmLck <rhmtofficial@gmail.com>
2021-06-05 23:50:25 +0700

    fixed, index.html, index.js, main.scss

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,6 @@ import '../styles/main.scss';
 import '../styles/responsive.scss';
 import main from './main';
 
-console.log('Hello Coders! :)'); 
 
 /**
  * Muncukan navbar ketika halaman di scroll ke bawah
